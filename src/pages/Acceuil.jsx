17200dd73e202c332ce9edf8f7934d31ee9b092f
Theo Foligne <halodino1@gmail.com>
2025-04-13 22:31:42 +0200

    banner cardlist 1240

--- src/pages/Acceuil.jsx
+++ src/pages/Acceuil.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import Banner from '../components/Banner';
 import image from '../img/BannerIMG.png';
 import CardList from '../components/CardList';
+import './Acceuil.scss';
 
 function Acceuil() {
     return (
-        <div>
+        <div className="home-container">
             <Banner image={image} text="Chez vous, partout et ailleurs" />
-            <CardList />
+            <div className="card-list-wrapper">
+                <CardList />
+            </div>
         </div>
     );
 }
 
 export default Acceuil;
-
